729c3067ed9d654da7588863aee8f932e481c0f1
Surabhil Ssk <surabhil.s.kumar@gmail.com>
2025-01-06 22:17:08 +0530

    Initiate animation

---
+++ frontend/src/App.tsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import { BackgroundGradientAnimation } from "./components/ui/background-gradient-animation";
+
+function App() {
+  return (
+    <div>
+      <BackgroundGradientAnimation />
+    </div>
+  );
+}
+
+export default App;

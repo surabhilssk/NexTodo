18614a1d8cfb645adfe348966a0c5eb5b1eeb67b
Surabhil Ssk <surabhil.s.kumar@gmail.com>
2025-01-09 22:10:08 +0530

    Update npm module for update validation

--- common/src/index.ts
+++ common/src/index.ts
@@ -37,6 +37,7 @@ export type TodoCreateType = z.infer<typeof todoCreateSchema>;
 export const updateTodoSchema = z.object({
     title: z.string().optional(),
     description: z.string().optional(),
+    completed: z.boolean().optional(),
 });
 
 export type UpdateTodoType = z.infer<typeof updateTodoSchema>;

004008dabb7c47e391ac70a96b971ab49f92c1ff
Surabhil Ssk <surabhil.s.kumar@gmail.com>
2025-01-14 16:20:44 +0530

    Implement redirect

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,15 +1,33 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import { Signup } from "./pages/Signup";
 import { Signin } from "./pages/Signin";
 import { Tasks } from "./pages/Tasks";
 
 function App() {
+  const handleRoute = () => {
+    const token = localStorage.getItem("token");
+    if (!token) {
+      return false;
+    } else {
+      return true;
+    }
+  };
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/signup" element={<Signup />} />
         <Route path="/signin" element={<Signin />} />
         <Route path="/tasks" element={<Tasks />} />
+        <Route
+          path="/"
+          element={
+            handleRoute() ? (
+              <Navigate to={"/tasks"} />
+            ) : (
+              <Navigate to={"/signin"} />
+            )
+          }
+        />
       </Routes>
     </BrowserRouter>
   );
